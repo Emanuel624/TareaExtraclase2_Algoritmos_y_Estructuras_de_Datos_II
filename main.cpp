f7c67404a8a3bcf73cfe1b590e70e421c143021b
Emanuel624 <emanuel.chavarria.hernandez@gmail.com>
2024-09-17 01:01:11 -0600

    Configuracion basica para CMakeLists.txt asi como una implementacion basica de la ventana por medio de QT

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <QApplication>
+#include <QWidget>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);  // Crear la aplicación
+
+    QWidget window;  // Crear una ventana
+    window.resize(400, 350);  // Definir el tamaño de la ventana
+    window.setWindowTitle("Ventana básica en Qt");  // Título de la ventana
+    window.show();  // Mostrar la ventana
+
+    return app.exec();  // Ejecutar el ciclo de eventos de Qt
 }
